b93ade4c37d7b5dd9aada7b94085ac9a17abf0c3
MooMooCow719 <loctran7402@gmail.com>
2025-06-23 22:50:20 -0700

    a

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementsByClassName("diffbutton").addEventListener("click", function() {
+      document.getElementById("start-page").style.display = "none";
+      document.getElementById("game-page").style.display = "block";
+});
